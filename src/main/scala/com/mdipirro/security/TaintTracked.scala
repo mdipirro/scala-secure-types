d737a2518097bfe25aa9b75e10da2a56cf82341f
Matteo Di Pirro <matteodipirro@gmail.com>
2025-11-11 12:42:20 +0100

    Make TaintTracked creation clearer
    This commit opinionates the TaintTracked#apply method, as it
    now returns a Tainted value by default. Furthermore, a new
    method, TaintTracked.unsafe, allows for the creation of Pure
    values. Sanitised values can only be obtained through
    sanitisation.

--- src/main/scala/com/mdipirro/security/TaintTracked.scala
+++ src/main/scala/com/mdipirro/security/TaintTracked.scala
@@ -25,32 +25,35 @@ class TaintTracked[P <: TaintLevel, +A] private(computeValue: () => A):
 
   def open(using CanOpen[P]): A = value
 
-  def map[B](f: A => B): TaintTracked[P, B] = TaintTracked(f(value))
+  def map[B](f: A => B): TaintTracked[P, B] = new TaintTracked(() => f(value))
 
   def flatMap[P1 <: TaintLevel, B](f: A => TaintTracked[P1, B]): TaintTracked[TaintPropagation[P, P1], B] =
     val result = f(value)
-    TaintTracked(result.value)
+    new TaintTracked(() => result.value)
 
   def foreach(f: A => Unit): Unit = f(value)
 
   def sanitise[B, E](s: A => Either[E, B]): Sanitised[B, E] =
     val result = s(value)
-    result.map(TaintTracked.apply)
+    result.map(r => new TaintTracked(() => r))
 
 object TaintTracked:
   type Sanitised[A, E] = Either[E, TaintTracked[TaintLevel.Sanitised.type, A]]
 
-  def apply[P <: TaintLevel, A](a: => A): TaintTracked[P, A] = new TaintTracked(() => a)
+  def apply[A](a: => A): TaintTracked[TaintLevel.Tainted.type, A] = new TaintTracked(() => a)
+
+  // Creates a Pure TaintTracked value explicitly
+  def unsafe[A](a: => A): TaintTracked[TaintLevel.Pure.type, A] = new TaintTracked(() => a)
 
 @main def sanitise(): Unit =
   print("Enter a value: ")
 
-  val tainted = TaintTracked[TaintLevel.Tainted.type, String](scala.io.StdIn.readLine())
+  val tainted = TaintTracked(scala.io.StdIn.readLine())
   val sanitised = tainted sanitise { str =>
     str.toIntOption.toRight(s"$str is not a number")
   }
 
-  val safeIncrement = TaintTracked[TaintLevel.Pure.type, Int](5)
+  val safeIncrement = TaintTracked.unsafe(5)
 
   sanitised match
     case Left(error) => println(s"Sanitisation failed: $error")
